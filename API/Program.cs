cb0a79b3b305fea22ab600c8017ed5134a870d62
Joffrey Lagut <j.lagut@live.fr>
2024-11-17 19:24:48 +0100

    feat: add token bearer authentication to SwaggerUI

--- API/Program.cs
+++ API/Program.cs
@@ -22,11 +22,35 @@ builder.Services.AddSwaggerGen(c =>
         new OpenApiInfo
         {
             Title = "Product Classification API",
+            Description = "Use deep learning model to categorize your products.",
             Version = "v1"
         }
     );
 
     c.IncludeXmlComments(Assembly.GetExecutingAssembly());
+    
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
 });
 
 builder.Services.AddIdentity<User, Role>(opt =>
