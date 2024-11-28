f50fb25021b19279468ce538e633a2d65a4ac138
Joffrey Lagut <j.lagut@live.fr>
2024-11-28 11:18:46 +0100

    feat(front): set url queries in UsersManagement

--- frontend/src/app/user/users-management/users-management.component.ts
+++ frontend/src/app/user/users-management/users-management.component.ts
@@ -3,13 +3,17 @@ import { SvgIconComponent } from "angular-svg-icon";
 import { debounceTime, Subscription } from "rxjs";
 import { UserService } from "../user.service";
 import { FormControl, ReactiveFormsModule } from "@angular/forms";
-import { Router } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import {
   ADMIN_ADD_USER_ROUTE,
   ADMIN_EDIT_USER_ROUTE,
+  ADMIN_USERS_ROUTE,
 } from "../../app.static-data";
 import { PaginatorComponent } from "../../shared/paginator/paginator.component";
+import { Location } from "@angular/common";
 
+// TODO: handle the case where the page in the query doesn't exist.
+//  For example: page=100 but there is only 1 page
 @Component({
   selector: "app-users-management",
   imports: [SvgIconComponent, ReactiveFormsModule, PaginatorComponent],
@@ -20,6 +24,7 @@ export class UsersManagementComponent implements OnDestroy {
   private getUserListSubscription: Subscription | null = null;
 
   currentPage = signal<number>(1);
+
   search = new FormControl("");
 
   userService: UserService;
@@ -41,18 +46,37 @@ export class UsersManagementComponent implements OnDestroy {
 
   constructor(
     userService: UserService,
+    route: ActivatedRoute,
     private router: Router,
+    private location: Location,
   ) {
     this.userService = userService;
+
+    // Set values from url query params
+    this.currentPage.set(parseInt(route.snapshot.queryParams["page"] ?? "1"));
+    this.search.setValue(route.snapshot.queryParams["search"] ?? "");
+
     this.refreshUserList();
-    this.search.valueChanges
-      .pipe(debounceTime(1000))
-      .subscribe((_) => this.refreshUserList());
+
+    // Set valueChange on the search input and trigger the event after 1s \
+    // of inactivity
+    this.search.valueChanges.pipe(debounceTime(1000)).subscribe((_) => {
+      this.refreshUserList();
+      this.updateUrlQuery();
+    });
   }
 
   changePage(pageNumber: number) {
     this.currentPage.set(pageNumber);
+    this.updateUrlQuery();
     this.refreshUserList();
+  }
+
+  updateUrlQuery() {
+    this.location.replaceState(
+      ADMIN_USERS_ROUTE,
+      `page=${this.currentPage()}&search=${this.search.value}`,
+    );
   }
 
   addUser() {
