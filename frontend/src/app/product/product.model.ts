1928f9f3db306d9e9b1fe47048e6e4741762bf4f
Joffrey Lagut <j.lagut@live.fr>
2024-12-07 17:06:06 +0100

    refactor(front): create a component to manage the product form
    - This component is currently used in edit-product, but it'll also be used in the prediction view.
    - Fix an issue with the form where the fields would not be highlighted in red when there was validation errors.

--- frontend/src/app/product/product.model.ts
+++ frontend/src/app/product/product.model.ts
@@ -17,6 +17,27 @@ export interface Product {
 }
 
 /**
+ * Represents the input values for product-related data.
+ *
+ * This interface is used to define the structure of product data that is submitted
+ * to or received from a form. It includes basic product information such as the
+ * product's ID, designation, description, and associated category ID. All fields
+ * are optional, allowing partial updates or submissions as needed.
+ *
+ * Properties:
+ * @property {number} [id] - The unique identifier for the product.
+ * @property {string} [designation] - The title or name of the product.
+ * @property {string} [description] - Detailed information about the product.
+ * @property {number} [categoryId] - The identifier for the category to which the product belongs.
+ */
+export interface ProductForm {
+  id?: number;
+  designation?: string;
+  description?: string;
+  categoryId?: number;
+}
+
+/**
  *  Defines the content of the payload expected to send a patch request
  *  to update a product on the API.
  */
