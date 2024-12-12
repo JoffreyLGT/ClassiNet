2efd26f55de11b48986f88f9ca8cad631a0de1b2
Joffrey Lagut <j.lagut@live.fr>
2024-12-12 16:43:11 +0100

    feat(front): add footer
    Signed-off-by: Joffrey Lagut <j.lagut@live.fr>

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from "@angular/router";
 import { SidebarComponent } from "./shared/sidebar/sidebar.component";
 import { NavbarComponent } from "./shared/navbar/navbar.component";
 import { ThemeManagerService } from "./shared/theme-manager/theme-manager.service";
+import { FooterComponent } from "./shared/footer/footer.component";
 
 @Component({
   selector: "app-root",
-  imports: [RouterOutlet, SidebarComponent, NavbarComponent],
+  imports: [RouterOutlet, SidebarComponent, NavbarComponent, FooterComponent],
   templateUrl: "./app.component.html",
 })
 export class AppComponent {
